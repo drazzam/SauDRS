cbe4d6ba49cc2bae37506fd04ea1dd27062c93ea
drazzam <ahmedyazzam@yahoo.com>
2026-03-27 02:07:32 +0200

    feat(model): add barrel exports

---
+++ src/lib/model/index.ts
@@ -0,0 +1,4 @@
+export { predict } from './predict';
+export { computeSRS, getRiskBand, srsToProb, BANDS } from './scoring';
+export { waldLogitCI } from './confidenceInterval';
+export { validateInputs, hasHardErrors, checkDiabetesGate } from './validate';
